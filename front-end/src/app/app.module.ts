e524f769ee91998b0eb4f4307139692cc4262174
kushal-borana <boranakushal19@gmail.com>
2022-09-20 14:05:08 +0530

    msg

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
